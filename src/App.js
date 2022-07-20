7376b80d0fb2abbfe63dce6d40098fbc3b8c71b2
Bindiya Patel <77851285+bindiyapatel@users.noreply.github.com>
2022-07-21 01:43:20 +0530

    Routes defined

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import UsersList from "./pages/UsersList";
+import AddUser from "./pages/AddUser";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<UsersList />}></Route>
+        <Route path="/addUser" element={<AddUser />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
